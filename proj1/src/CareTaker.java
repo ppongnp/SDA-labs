90f70a6678ed662986c2b2eb864d0f262adcc13d
ppongnp <ppgxnp@gmail.com>
2020-03-08 16:23:58 +0700

    add serialize function

--- proj1/src/CareTaker.java
+++ proj1/src/CareTaker.java
@@ -1,6 +1,7 @@
 import java.util.*;
+import java.io.*;
 
-public class CareTaker {
+public class CareTaker implements java.io.Serializable {
     private List<Memento> mementoList = new ArrayList<Memento>();
 
     public void add(Memento state){
@@ -10,4 +11,41 @@ public class CareTaker {
     public Memento get(int index){
         return mementoList.get(index);
     }
+
+    public void saveSerialize(String filename){
+        try {
+
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+
+            for (Memento m:mementoList) {
+               out.writeObject(m);
+            }
+
+            out.close();
+            file.close();
+            System.out.println("Object has been serialized");
+
+        }catch(IOException ex) {
+            System.out.println("IOException is caught");
+        }
+    }
+
+    public void deserialize(String filename){
+        try {
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            String object_list = in.readObject().toString();
+            in.close();
+            file.close();
+
+            System.out.println(object_list);
+
+        }catch(IOException ex) {
+            System.out.println("IOException is caught");
+        }catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            }
+        }
 }
