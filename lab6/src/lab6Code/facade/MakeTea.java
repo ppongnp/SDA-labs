b737c03a66cc50b109b142dd1a0a58f916438aa7
ppongnp <ppgxnp@gmail.com>
2020-03-16 01:21:05 +0700

    finish second question

---
+++ lab6/src/lab6Code/facade/MakeTea.java
@@ -0,0 +1,11 @@
+package lab6Code.facade;
+
+public class MakeTea {
+    public static void main(String[] args) {
+        TeaCup blueCup = new TeaCup();
+        Water water = new Water();
+        TeaInfuser infuser = new TeaInfuser();
+        TeaFacade teaMaker = new TeaFacade(blueCup,water,infuser);
+        teaMaker.makeTea("Earl Grey");
+    }
+}
---
+++ lab6/src/lab6Code/facade/TeaFacade.java
@@ -0,0 +1,21 @@
+package lab6Code.facade;
+
+public class TeaFacade {
+    private TeaCup cup;
+    private Water water;
+    private TeaInfuser infuser;
+
+    public TeaFacade(TeaCup cup,Water water,TeaInfuser infuser){
+        this.cup = cup;
+        this.water = water;
+        this.infuser = infuser;
+    }
+    public void makeTea(String teaType){
+        Tea tea = new Tea(teaType);
+        infuser.addTea(tea);
+        water.boilWater();
+        cup.addWater(water);
+        cup.setSteepTime(15);
+        cup.steep();
+    }
+}
